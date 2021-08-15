c71c820709ef9c9358fd175d3bc593048fd2c205
Chuhj <chj9574@naver.com>
2021-08-15 18:49:29 +0900

    2490 - 윷놀이

---
+++ baekjoon/2490.py
@@ -0,0 +1,12 @@
+for _ in range(3):
+  c = input().count('0')
+  if c == 0:
+    print('E')
+  elif c == 1:
+    print('A')
+  elif c == 2:
+    print('B')
+  elif c == 3:
+    print('C')
+  elif c == 4:
+    print('D')
