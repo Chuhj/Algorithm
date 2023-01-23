c67590c70c604dea627703f3d206c7f605c91f70
Chuhj <chj9574@naver.com>
2023-01-23 20:25:10 +0900

    16926 - 배열 돌리기 1 주석 추가

--- baekjoon/javascript/16926.js
+++ baekjoon/javascript/16926.js
@@ -13,21 +13,26 @@ for (let i = 0; i < rectNum; i++) {
     const row = n - i - 1;
     const col = m - i - 1;
 
-    const temp = array[row][i];
+    const temp = array[row][i]; // 현재 돌리는 사각형의 좌측 하단의 값
 
     for (let r = row; r > i; r--) {
+      // 왼쪽 변을 밑으로 옮김
       array[r][i] = array[r - 1][i];
     }
     for (let c = i; c < col; c++) {
+      // 위쪽 변을 왼쪽으로 옮김
       array[i][c] = array[i][c + 1];
     }
     for (let r = i; r < row; r++) {
+      // 오른쪽 변을 위쪽으로 옮김
       array[r][col] = array[r + 1][col];
     }
     for (let c = col; c > 1 + i; c--) {
+      // 밑쪽 변을 오른쪽으로 옮김
       array[row][c] = array[row][c - 1];
     }
-    array[row][1 + i] = temp;
+
+    array[row][1 + i] = temp; // 빠져있던 값을 원 위치에 넣어줌
   }
 }
 
